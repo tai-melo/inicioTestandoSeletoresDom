5d1abbebb9be7b7dea8e76c105ec1964b8837605
Tairine de Melo <tairine.me@gmail.com>
2022-12-14 19:39:11 -0300

    inserindo defer no script e querySelector

--- script-teste.js
+++ script-teste.js
@@ -1,6 +1,15 @@
+// const outrotitulo = document
+// console.log(outrotitulo)
+
 const titulo = document.getElementById("titulo-principal");
 
 console.log(titulo)
 
 const textos = document.getElementsByClassName("texto-simples");
+
+console.log(textos);
+
 console.log(textos[0]);
+
+const segundoTitulo = document.querySelector("div h2");
+console.log(segundoTitulo)
